3455c60029044a189411f6145f3bb241ff711304
sollent <pavel.laikov98@gmail.com>
2021-10-25 22:11:06 +0300

    Add bundle composer.json settings

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace sollent\DoctrineSmartBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        return new TreeBuilder('doctrine_smart_bundle');
+    }
+}
